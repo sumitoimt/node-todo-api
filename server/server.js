4bb6da3c68481e18913e0e37f7a93d21ebc431c5
sumitoimt <sumitoimt@gmail.com>
2018-03-19 12:31:07 +0530

    Add post todos route monooes

---
+++ server/server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var {mongoose} = require('./db/mongoose.js');
+
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+app.use(bodyParser.json());
+app.post('/todos',(req,res)=>{
+	var todo = new Todo({
+		text:req.body.text
+	});
+	todo.save().then((doc)=>{
+	res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+//Get /todos/123
+
+
+
+app.listen(3000, ()=>{
+	console.log('Started on port 3000');
+});
+
+/*var newTodo = new Todo({
+	text:'Cook dinner'
+});
+
+newTodo.save().then((doc)=>{
+	console.log('Save todo',doc);
+},(e)=>{
+	console.log('Unable to save');
+});*/
+
+/*var otherTodo = new Todo({
+	text:'something to do'
+});
+
+otherTodo.save().then((doc)=>{
+	console.log(JSON.stringify(doc,undefined,2));
+},(e)=>{
+	console.log('Unable to save');
+});*/
+
+// user model
+
+//Email Property
+
+
+
+/*var user = new User({
+email:'sumitoimt@gmail.com'
+});
+
+user.save().then((doc)=>{
+	console.log('User Saved',doc);
+},(e)=>{
+	console.log('Unable to Save');
+});*/
