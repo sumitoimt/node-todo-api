9f3f8c35d85a778a56319acd804530b7688ebe6a
sumitoimt <sumitoimt@gmail.com>
2018-03-19 13:57:50 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,13 @@ app.post('/todos',(req,res)=>{
 });
 
 //Get /todos/123
-
+app.get('/todos/',(req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos});
+	},(e)=>{
+		res.status(400).send(e);
+	})
+})
 
 
 app.listen(3000, ()=>{
