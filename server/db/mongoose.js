3605b8ed414faa7c2d81e2cae99a0a79b80fee2d
sumitoimt <sumitoimt@gmail.com>
2018-03-22 15:44:06 +0530

    setup seprate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.PROD_MONGODB);
 
 module.exports = {mongoose};
+
